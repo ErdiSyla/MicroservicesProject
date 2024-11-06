b13e0638de8c3f2ae00780b8c3a4e659d14fec57
ErdiSyla <erdisyla6@gmail.com>
2024-11-06 04:38:41 +0100

    Added loggin to FraudController so it logs every fraud-check-history request

--- fraud/src/main/java/com/erdi/fraud/controller/FraudController.java
+++ fraud/src/main/java/com/erdi/fraud/controller/FraudController.java
@@ -7,16 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudController {
     private final FraudCheckService fraudCheckService;
 
     @GetMapping(path ="{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId){
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("Fraud check request for customer {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
